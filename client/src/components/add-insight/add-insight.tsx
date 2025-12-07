0c3cae1038651403285cd0a05e7aef0261715e63
Amanda Hasenzahl <amandahasenzahl@Amandas-MacBook-Pro.local>
2025-12-07 14:15:15 -0700

    Add onchanges for modal form fields and a label for select field

--- client/src/components/add-insight/add-insight.tsx
+++ client/src/components/add-insight/add-insight.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BRANDS } from "../../lib/consts.ts";
 import { Button } from "../button/button.tsx";
 import { Modal, type ModalProps } from "../modal/modal.tsx";
@@ -6,14 +7,29 @@ import styles from "./add-insight.module.css";
 type AddInsightProps = ModalProps;
 
 export const AddInsight = (props: AddInsightProps) => {
+  const [brandValue, setBrandValue] = useState<number>(BRANDS[0].id);
+  const [insightText, setInsightText] = useState<string>("");
+
   const addInsight = () => undefined;
+
+  const handleBrandChange = (e) => {
+    setBrandValue(e.target.value);
+  };
+
+  const handleInsightText = (e) => {
+    setInsightText(e.target.value);
+  };
 
   return (
     <Modal {...props}>
       <h1 className={styles.heading}>Add a new insight</h1>
       <form className={styles.form} onSubmit={addInsight}>
         <label className={styles.field}>
-          <select className={styles["field-input"]}>
+          Brand
+          <select
+            className={styles["field-input"]}
+            onChange={handleBrandChange}
+          >
             {BRANDS.map(({ id, name }) => (
               <option value={id} key={id}>{name}</option>
             ))}
@@ -25,6 +41,7 @@ export const AddInsight = (props: AddInsightProps) => {
             className={styles["field-input"]}
             rows={5}
             placeholder="Something insightful..."
+            onChange={handleInsightText}
           />
         </label>
         <Button className={styles.submit} type="submit" label="Add insight" />
